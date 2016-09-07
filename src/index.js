3d0dc1e6bf57135c4e8e1954b4d254d8eaf1e79d
Victor Zamfir <victor.zamfir@bengroup.com>
2016-09-07 16:50:41 +0300

    better exports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-export generateCRUDServices from './generateCRUDServices';
+import generateCRUDServices from './generateCRUDServices';
+import Store from './Store';
+import utils from './utils';
+
+export {
+  generateCRUDServices,
+  Store,
+  utils,
+};
